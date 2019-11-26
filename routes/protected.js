bc950d7ec93cced06c70d64133ea4361ec49d2d3
merrymustard <merryymustard@gmail.com>
2019-11-25 19:36:42 -0600

    cloudifary settings

--- routes/protected.js
+++ routes/protected.js
@@ -5,46 +5,54 @@ const axios = require('axios')
 
 
 //VISTA DE TODOS LOS LIBROS
-router.get('/index', (_, res)=> {
+router.get('/index', (_, res) => {
   const buks = books.find()
   res.render('user/index', buks)
 })
 
 //DETAIL
 
-router.get('/:id', (req, res)=> {
-  const { id } = req.params;
+router.get('/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
   const selBook = books.findById(id)
   res.render('user/detail', selBook)
 })
 
 //CONFIRMATION
 
-router.get('/confirmation', (_, res)=>{
+router.get('/confirmation', (_, res) => {
   res.render('user/confirmation')
 });
 
 //USER PROFILE
 
-router.get('/profile', (_, res)=> {
-  const {id} = req.user = id
+router.get('/profile', (_, res) => {
+  const {
+    id
+  } = req.user = id
   res.render('user/profile', user)
 })
 
 //ADD NEW
 
-router.get('/new', (_, res)=> {
+router.get('/new', (_, res) => {
   res.render('user/new')
 })
 
 router.post('/new/ISBN', async (req, res) => {
-  const { ISBN10 } = req.body;
-  const { id } = req.user;
+  const {
+    ISBN10
+  } = req.body;
+  const {
+    id
+  } = req.user;
   const buuk = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=+isbn:${ISBN10}&key=${g_key}`)
   const buk = {
-    title:buuk.volumeInfo.title,
-    description:buuk.volumeInfo.description,
-    author:buuk.volumeInfo.authors,
+    title: buuk.volumeInfo.title,
+    description: buuk.volumeInfo.description,
+    author: buuk.volumeInfo.authors,
     pages: buuk.volumeInfo.pageCount,
     categories: buuk.volumeInfo.categories,
     pic: buuk.volumeInfo.imageLinks.thumbnail,
@@ -54,11 +62,13 @@ router.post('/new/ISBN', async (req, res) => {
     swapper: id
   }
   books.create(buk)
-  res.redirect('/user/confirmation') 
+  res.redirect('/user/confirmation')
 })
 
 router.post('/new/form', async (req, res) => {
-  const { id } = req.user;
+  const {
+    id
+  } = req.user;
   const {
     title,
     description,
@@ -68,21 +78,21 @@ router.post('/new/form', async (req, res) => {
     pic,
     idioma,
     publisher,
-    publishedDate} = req.body;
+    publishedDate
+  } = req.body;
 
   const buk = {
-    title:title,
-    description:description,
-    author:author,
-    pages:pages,
-    categories:categories,
-    pic:pic,
-    idioma:idioma,
-    publisher:publisher,
-    publishDate:publishedDate,
-    swapper:id
+    title: title,
+    description: description,
+    author: author,
+    pages: pages,
+    categories: categories,
+    pic: pic,
+    idioma: idioma,
+    publisher: publisher,
+    publishDate: publishedDate,
+    swapper: id
   }
   books.create(buk)
-  res.redirect('/user/confirmation') 
+  res.redirect('/user/confirmation')
 })
-
