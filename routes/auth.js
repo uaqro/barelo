c50518859d79f2959678e8189d317ca3d70df4f9
merrymustard <merryymustard@gmail.com>
2019-11-26 16:57:48 -0600

    simple login work

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
+const uploadCloud = require('../config/cloudinary.js');
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -30,7 +31,9 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", uploadCloud.single("photo"), (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  const { secure_url } = req.file;
+  // const {
+  //   secure_url
+  // } = req.file;
   const {
     address,
     lng,
@@ -59,7 +62,7 @@ router.post("/signup", uploadCloud.single("photo"), (req, res, next) => {
     const newUser = new User({
       username,
       password: password,
-      photoURL: secure_url,
+      // photoURL: secure_url,
       place: {
         address: address,
         coordinates: [lng, ltd]
@@ -69,7 +72,7 @@ router.post("/signup", uploadCloud.single("photo"), (req, res, next) => {
     newUser
       .save()
       .then(() => {
-        res.redirect("/");
+        res.redirect("/user/index");
       })
       .catch(err => {
         res.render("auth/signup", {
