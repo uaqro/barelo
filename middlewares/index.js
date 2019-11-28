2c70ce7d920ceecfb113ae0b38ae17e92d0fe712
merrymustard <merryymustard@gmail.com>
2019-11-28 16:54:58 -0600

    update book location page finished

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,7 @@
 exports.checkUser = (req, res, next) => {
   if (req.isAuthenticated()) {
     req.app.locals.logged = true;
+    req.app.locals.value = req.user.tokens;
   } else {
     req.app.locals.logged = false;
   }
