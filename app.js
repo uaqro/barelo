06893416867e54aeb778a6f9bfbbf78d142c8f13
merrymustard <merryymustard@gmail.com>
2019-11-27 21:01:32 -0600

    issues solved

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ const {
 } = require("./middlewares/index");
 
 mongoose
-  // .connect("mongodb://localhost/barelo", {
-  //   useNewUrlParser: true
-  // })
-  .connect(process.env.DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  .connect("mongodb://localhost/barelo", {
+    useNewUrlParser: true
   })
+  // .connect(process.env.DB, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true
+  // })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
