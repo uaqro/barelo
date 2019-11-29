5515cd7b643cbaa7df1fa84ef38b38ee8ab8ef3a
merrymustard <merryymustard@gmail.com>
2019-11-29 14:42:47 -0600

    changin dv env

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const {
 } = require("./middlewares/index");
 
 mongoose
-  .connect("mongodb://localhost/barelo", {
-    useNewUrlParser: true
-  })
+  //   .connect("mongodb://localhost/barelo", {
+  //     useNewUrlParser: true
+  //   })
 
-  // .connect(process.env.DB, {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true
-  // })
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
