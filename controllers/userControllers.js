389de7fe9651eecf8058542c59782ab38c39654e
Uli <uaqro@protonmail.com>
2019-11-28 22:28:58 -0600

    change

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,9 +6,7 @@ require("dotenv").config();
 
 //FUNCIONES PÁGINA
 exports.indexGet = async (req, res) => {
-  const {
-    place
-  } = req.user;
+  const { place } = req.user;
   const buks = await books.find({
     place: {
       $nearSphere: {
@@ -20,31 +18,19 @@ exports.indexGet = async (req, res) => {
       }
     },
     picked: false
-<<<<<<< HEAD
   });
   res.render("user/index", {
     buks
-=======
->>>>>>> 82de211f7cbe9e5e1145a6cd2063d13cc219f771
   });
 };
 // CRUD LIBROS
 exports.ISBNform = async (req, res) => {
-  const {
-    ISBN10,
-    lng,
-    lat,
-    address
-  } = req.body;
-  const {
-    id
-  } = req.user;
+  const { ISBN10, lng, lat, address } = req.body;
+  const { id } = req.user;
   const buuk = await axios.get(
     `https://www.googleapis.com/books/v1/volumes?q=+isbn:${ISBN10}&key=${process.env.g_key}`
   );
-  const {
-    secure_url
-  } = req.file;
+  const { secure_url } = req.file;
   const buk = {
     title: buuk.data.items[0].volumeInfo.title,
     description: buuk.data.items[0].volumeInfo.description,
@@ -71,45 +57,24 @@ exports.ISBNform = async (req, res) => {
   await res.redirect(`/user/${buki._id}/confirm`);
 };
 exports.confirmBook = async (req, res) => {
-  const {
-    id
-  } = req.params;
+  const { id } = req.params;
   const buk = await books.findById(id);
   console.log(buk);
   res.render("user/confirmation", buk);
 };
 exports.getpatchForm = async (req, res) => {
-<<<<<<< HEAD
   const { backURL } = req.header("Referer");
   const { id } = req.params;
-=======
-  const {
-    id
-  } = req.params;
->>>>>>> 5603edc916b88d0a8c6b65cd35844b2575be9258
   const buk = await books.findById(id);
   if (!buk.picked) {
     res.render("user/updateBook", { buk, backURL });
   }
 };
 exports.patchForm = async (req, res) => {
-<<<<<<< HEAD
   const { id, redirectRoute } = req.params;
   const { address, lng, lat } = req.body;
-=======
-  const {
-    id
-  } = req.params;
-  const {
-    address,
-    lng,
-    lat
-  } = req.body;
->>>>>>> 5603edc916b88d0a8c6b65cd35844b2575be9258
   if (req.file) {
-    const {
-      secure_url
-    } = req.file;
+    const { secure_url } = req.file;
     const buk = await books.findById(id);
     buk.place.address = address;
     buk.place.cooordinates = [lng, lat];
@@ -126,18 +91,9 @@ exports.patchForm = async (req, res) => {
   }
 };
 exports.deleteBook = async (req, res) => {
-<<<<<<< HEAD
   const { id } = req.params;
   const { _id } = req.user;
   const backURL = req.header("Referer");
-=======
-  const {
-    id
-  } = req.params;
-  const {
-    _id
-  } = req.user;
->>>>>>> 5603edc916b88d0a8c6b65cd35844b2575be9258
   const bookmodel = await books.findById(id);
   if (!bookmodel.picked) {
     await books.findByIdAndDelete(id);
@@ -154,12 +110,8 @@ exports.deleteBook = async (req, res) => {
   }
 };
 exports.delBook = async (req, res) => {
-  const {
-    id
-  } = req.params;
-  const {
-    _id
-  } = req.body;
+  const { id } = req.params;
+  const { _id } = req.body;
   await books.findByIdAndDelete(id);
   await user.findByIdAndUpdate(_id, {
     $pull: {
@@ -171,12 +123,8 @@ exports.delBook = async (req, res) => {
   res.redirect("/user/new");
 };
 exports.pickABook = async (req, res) => {
-  const {
-    _id
-  } = req.user;
-  const {
-    id
-  } = req.params;
+  const { _id } = req.user;
+  const { id } = req.params;
   const usr = await user.findById(_id);
   const showAlert = false;
   usr.pickedBooks.push(id);
@@ -189,12 +137,8 @@ exports.pickABook = async (req, res) => {
   res.redirect(`/user/${id}/book-details`);
 };
 exports.bookDetails = async (req, res) => {
-  const {
-    id
-  } = req.params;
-  const {
-    _id
-  } = req.user;
+  const { id } = req.params;
+  const { _id } = req.user;
   let showB = true;
   let showC = false;
   let showEdit = false;
@@ -227,12 +171,8 @@ exports.bookDetails = async (req, res) => {
 };
 exports.seeProfile = async (req, res) => {
   let commentsArray = [];
-  const {
-    id
-  } = req.params;
-  const {
-    _id
-  } = req.user;
+  const { id } = req.params;
+  const { _id } = req.user;
   const swapper = await user.findById(id).populate("commentsRec");
 
   await swapper.commentsRec.forEach(async e => {
@@ -254,9 +194,7 @@ exports.seeProfile = async (req, res) => {
 
 // router.get('/:id/delete-comment', deleteComment)
 exports.deleteComment = async (req, res) => {
-  const {
-    id
-  } = req.params; //id del comentario
+  const { id } = req.params; //id del comentario
   const com = await Comment.findById(id);
   await user.findByIdAndUpdate(com.swapperPosting, {
     $pull: {
@@ -276,34 +214,22 @@ exports.deleteComment = async (req, res) => {
   res.redirect(`/user/${com.swapperRec}/profile`);
 };
 exports.editComment = async (req, res) => {
-  const {
-    id
-  } = req.params; //Id del comentario
+  const { id } = req.params; //Id del comentario
   const com = await Comment.findById(id);
   res.render("user/editComment", com);
 };
 exports.patchComment = async (req, res) => {
-  const {
-    id
-  } = req.params;
-  const {
-    comment
-  } = req.body;
+  const { id } = req.params;
+  const { comment } = req.body;
   const com = await Comment.findById(id);
   com.body = comment;
   await com.save();
   res.redirect(`/user/${com.swapperRec}/profile`);
 };
 exports.newComment = async (req, res) => {
-  const {
-    _id
-  } = req.user;
-  const {
-    id
-  } = req.params;
-  const {
-    comment
-  } = req.body;
+  const { _id } = req.user;
+  const { id } = req.params;
+  const { comment } = req.body;
   const newComment = await Comment.create({
     body: comment,
     swapperPosting: _id,
@@ -320,43 +246,28 @@ exports.newComment = async (req, res) => {
 
 //CRUD USER
 exports.getProfile = async (req, res) => {
-  const {
-    id
-  } = req.user;
+  const { id } = req.user;
   const swapper = await user
     .findById(id)
     .populate("pickedBooks publishedBooks");
   res.render("user/ownProfile", swapper);
 };
 exports.deleteUser = async (req, res) => {
-  const {
-    id
-  } = req.user;
+  const { id } = req.user;
   req.logout();
   await user.findByIdAndDelete(id);
   res.redirect("/");
 };
 exports.editUser = async (req, res) => {
-  const {
-    id
-  } = req.user;
+  const { id } = req.user;
   const usr = await user.findById(id);
   res.render("user/updateProfile", usr);
 };
 exports.patchUser = async (req, res) => {
-  const {
-    id
-  } = req.user;
-  const {
-    address,
-    lng,
-    lat,
-    email
-  } = req.body;
+  const { id } = req.user;
+  const { address, lng, lat, email } = req.body;
   if (req.file) {
-    const {
-      secure_url
-    } = req.file;
+    const { secure_url } = req.file;
     const updt = {
       place: {
         address,
@@ -381,12 +292,8 @@ exports.patchUser = async (req, res) => {
 };
 //NO SE ESTÁ USANDO
 exports.postForm = async (req, res) => {
-  const {
-    id
-  } = req.user;
-  const {
-    secure_url
-  } = req.file;
+  const { id } = req.user;
+  const { secure_url } = req.file;
   const {
     title,
     description,
